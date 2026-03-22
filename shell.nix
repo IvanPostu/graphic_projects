5c96f44a1cdd1611be7cf7320f91245ab3c99c32
IvanPostu <ipostu20000127@gmail.com>
2026-03-22 07:58:59 +0200

    chore: Init project

---
+++ shell.nix
@@ -0,0 +1,27 @@
+let 
+  pkgs = import <nixpkgs> { config = { allowUnfree = false; }; };
+  PROJECT_ROOT = builtins.toString ./.;
+in
+pkgs.mkShell {
+  name = "app-shell";
+
+  nativeBuildInputs = [
+    pkgs.pkg-config # is required for `cargo install sqlx-cli`
+  ];
+
+  buildInputs = with pkgs; [
+    rustup
+    gcc14
+    clang-tools
+  ];
+
+  LANG = "en_US.UTF-8";
+  LC_ALL = "en_US.UTF-8";
+
+  shellHook = ''
+        export PROJECT_ROOT="${PROJECT_ROOT}"
+        export RUSTUP_HOME="$PROJECT_ROOT/.rustup"
+        export CARGO_HOME="$PROJECT_ROOT/.cargo"
+        export PATH="$CARGO_HOME/bin:$PATH"
+  '';
+}
