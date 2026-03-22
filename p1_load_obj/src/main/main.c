deadf344db962e386bc7e025b6725dccd47c3aba
IvanPostu <ipostu20000127@gmail.com>
2026-03-22 08:34:25 +0200

    chore: setup p1 project

---
+++ p1_load_obj/src/main/main.c
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+int main() {
+  printf("Test %d\n", 1);
+  return 0;
+}
--- shell.nix
+++ shell.nix
@@ -13,12 +13,19 @@ pkgs.mkShell {
     rustup
     gcc14
     clang-tools
+    cmake
+    libx11 # needed to build raylib
+    libxrandr # needed to build raylib
+    libxinerama # needed to build raylib
+    libxcursor # needed to build raylib
+    libxi # needed to build raylib
   ];
 
   LANG = "en_US.UTF-8";
   LC_ALL = "en_US.UTF-8";
 
   shellHook = ''
+        export QQQ=${pkgs.clang-tools}
         export PROJECT_ROOT="${PROJECT_ROOT}"
         export RUSTUP_HOME="$PROJECT_ROOT/.rustup"
         export CARGO_HOME="$PROJECT_ROOT/.cargo"
