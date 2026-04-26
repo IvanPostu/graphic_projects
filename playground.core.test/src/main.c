308ba1b424c11dafdb8693a56af076fe021bb0ad
IvanPostu <ipostu20000127@gmail.com>
2026-04-26 14:57:12 +0300

    chore: playground

--- playground.core.test/src/main.c
+++ playground.core.test/src/main.c
@@ -15,7 +15,7 @@ int main(void) {
   int nums1[] = {1, 2, 3};
   int nums2[] = {5, 7, 8};
   double r = findMedianSortedArrays(nums1, sizeof(nums1), nums2, sizeof(nums2));
-  printf("%f\n", r);
+  printf("%f\n", r + 2);
   test_manager_init();
 
   test_manager_register_test(test_1_plus_2_is_3, "Dummy test sum logic");
